bdc1fc8e250ff4765ef0038e48c85dd81d87233d
etozhedankor <41688602+DvnKor@users.noreply.github.com>
2021-05-12 18:35:08 +0500

    Больше пунктов меню

--- src/routing/DrawerNavigation.tsx
+++ src/routing/DrawerNavigation.tsx
@@ -10,7 +10,12 @@ import { useAppSelector } from '../redux/hooks';
 import { selectCurrentUser } from '../redux/users/selectors';
 import { Right } from '../redux/users/types';
 import SettingsIcon from '../components/icons/SettingsIcon';
-import RequestsIcon from "../components/icons/RequestsIcon";
+import RequestsIcon from '../components/icons/RequestsIcon';
+import ManagersIcon from '../components/icons/ManagersIcon';
+import ClientsIcon from '../components/icons/ClientsIcon';
+import MeetingsIcon from '../components/icons/MeetingsIcon';
+import ScheduleIcon from "../components/icons/ScheduleIcon";
+import RouteIcon from "../components/icons/RouteIcon";
 
 const Drawer = createDrawerNavigator();
 
@@ -30,7 +35,57 @@ const DrawerNavigation = () => {
                         title: 'Главная',
                     }}
                 />
-                {currentUserIsAdmin && (
+                <Drawer.Screen
+                    name={DrawerRoutes.Schedule}
+                    component={HomeScreen}
+                    options={{
+                        title: 'График',
+                        drawerIcon: ({ focused }) => (
+                            <ScheduleIcon focused={focused} />
+                        ),
+                    }}
+                />
+                <Drawer.Screen
+                    name={DrawerRoutes.Route}
+                    component={HomeScreen}
+                    options={{
+                        title: 'Маршрут',
+                        drawerIcon: ({ focused }) => (
+                            <RouteIcon focused={focused} />
+                        ),
+                    }}
+                />
+                {currentUserIsAdmin && [
+                    <Drawer.Screen
+                        name={DrawerRoutes.Managers}
+                        component={RequestsScreen}
+                        options={{
+                            title: 'Менеджеры',
+                            drawerIcon: ({ focused }) => (
+                                <ManagersIcon focused={focused} />
+                            ),
+                        }}
+                    />,
+                    <Drawer.Screen
+                        name={DrawerRoutes.Clients}
+                        component={RequestsScreen}
+                        options={{
+                            title: 'Клиенты',
+                            drawerIcon: ({ focused }) => (
+                                <ClientsIcon focused={focused} />
+                            ),
+                        }}
+                    />,
+                    <Drawer.Screen
+                        name={DrawerRoutes.Meetings}
+                        component={RequestsScreen}
+                        options={{
+                            title: 'Встречи',
+                            drawerIcon: ({ focused }) => (
+                                <MeetingsIcon focused={focused} />
+                            ),
+                        }}
+                    />,
                     <Drawer.Screen
                         name={DrawerRoutes.Requests}
                         component={RequestsScreen}
@@ -40,8 +95,8 @@ const DrawerNavigation = () => {
                                 <RequestsIcon focused={focused} />
                             ),
                         }}
-                    />
-                )}
+                    />,
+                ]}
                 <Drawer.Screen
                     name={DrawerRoutes.Options}
                     component={OptionsScreen}
--- src/routing/routes.ts
+++ src/routing/routes.ts
@@ -4,10 +4,22 @@ export enum DrawerRoutes {
     Home = 'Home',
     Options = 'Options',
     Requests = 'Requests',
+    Managers = 'Managers',
+    Clients = 'Clients',
+    Meetings = 'Meetings',
+    Schedule = 'Schedule',
+    Route = 'Route',
+    Map = 'Map',
 }
 
 export type RootDrawerParamList = {
     [DrawerRoutes.Home]: undefined;
     [DrawerRoutes.Options]: { user: User };
     [DrawerRoutes.Requests]: undefined;
+    [DrawerRoutes.Managers]: undefined;
+    [DrawerRoutes.Clients]: undefined;
+    [DrawerRoutes.Meetings]: undefined;
+    [DrawerRoutes.Schedule]: undefined;
+    [DrawerRoutes.Route]: undefined;
+    [DrawerRoutes.Map]: undefined;
 };
