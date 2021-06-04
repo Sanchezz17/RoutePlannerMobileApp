845c0a6994fc0a01394145b510a2cd5465743ab1
etozhedankor <41688602+DvnKor@users.noreply.github.com>
2021-06-04 21:57:22 +0500

    Переименовал приложение

--- ios/RoutePlannerTests/RoutePlannerTests.m
+++ ios/RoutePlannerTests/RoutePlannerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RoutePlannerMobileAppTests : XCTestCase
+@interface RoutePlannerTests : XCTestCase
 
 @end
 
-@implementation RoutePlannerMobileAppTests
+@implementation RoutePlannerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
