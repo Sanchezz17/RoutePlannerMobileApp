403d28f86002e2f6220a9899547c9898b0f6deb9
etozhedankor <41688602+DvnKor@users.noreply.github.com>
2021-06-16 19:33:46 +0500

    Экран входа в приложение

--- src/containers/AuthorizeRoute/AuthorizeRoute.styles.tsx
+++ src/containers/AuthorizeRoute/AuthorizeRoute.styles.tsx
@@ -1,13 +1,31 @@
 import { StyleSheet } from 'react-native';
 
+import { PaletteStorage } from '../../components/palette/PaletteStorage';
+
+const palette = PaletteStorage.getPalette();
 export default StyleSheet.create({
     view: {
         height: '100%',
         display: 'flex',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
     },
     signInButton: {
+        marginTop: 70,
         height: '10%',
+    },
+    logo: {
+        marginTop: 100,
+    },
+    title: {
+        fontSize: 26,
+        marginTop: 20,
+    },
+    text: {
+        fontSize: 16,
+        marginTop: 15,
+        width: '85%',
+        textAlign: 'center',
+        alignSelf: 'center',
     },
 });
--- src/containers/AuthorizeRoute/AuthorizeRoute.tsx
+++ src/containers/AuthorizeRoute/AuthorizeRoute.tsx
@@ -1,16 +1,20 @@
 import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { View } from 'react-native';
+import { Text, Title } from 'react-native-paper';
 
 import {
     signIn,
     trySignInSilently,
 } from '../../common/authorization/google/authStateManager';
+import EntryLogo from '../../components/icons/Screens/EntryLogo';
+import { PaletteStorage } from '../../components/palette/PaletteStorage';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { selectCurrentUser } from '../../redux/users/selectors';
 import { getCurrentUserThunk } from '../../redux/users/thunks';
 import styles from './AuthorizeRoute.styles';
 
+const palette = PaletteStorage.getPalette();
 export const AuthorizeRoute: FunctionComponent = ({ children }) => {
     const dispatch = useAppDispatch();
     const currentUser = useAppSelector(selectCurrentUser);
@@ -50,6 +54,11 @@ export const AuthorizeRoute: FunctionComponent = ({ children }) => {
         <>{children}</>
     ) : (
         <View style={styles.view}>
+            <EntryLogo style={styles.logo} />
+            <Title style={styles.title}>RoutePlanner</Title>
+            <Text style={styles.text}>
+                Приложение для составления удобных маршрутов для встреч
+            </Text>
             <GoogleSigninButton
                 style={styles.signInButton}
                 size={GoogleSigninButton.Size.Wide}
